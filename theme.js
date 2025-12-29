3c774e9adc56403cc4ae51af18adee43cbafca9f
Nathalie <nannaskrutt@gmail.com>
2025-12-29 11:36:41 +0100

    buttons now working on clicks by assigning querySelector to a variable

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-querySelectorAll('.theme-btn');
+const themeButtons = document.querySelectorAll('.theme-btn');
 
 //Event listeners for theme buttons
 themeButtons.forEach(button => {
@@ -10,11 +10,18 @@ themeButtons.forEach(button => {
 
 // Function to switch themes
 function switchTheme(theme) {
-    document.documentElement.removeAttribute('data-theme'); // Remove existing data-theme attribute
-    document.documentElement.setAttribute('data-theme', theme); // Set data-theme attribute on root element
-    document.body.className = theme + '-theme'; // Update body class to match theme
+    // Remove existing theme classes
+    document.body.className = '';
+
+    //Apply new theme if it's not purple (default)
+    if (theme !== 'purple') {
+        document.body.classList.add(theme + '-theme'); // Add selected theme class to body
+    }
+
+    saveTheme(theme); // Save selected theme in cookie
 }
 
 function saveTheme(theme) {
-    document.cookie = "selectedTheme" + theme + "; path=/; max-age=" + 60*60*24*30; // Save theme in cookie for 30 days
+    document.cookie = "selectedTheme="+ theme + "; path=/; max-age=" + 60*60*24*30; // Save theme in cookie for 30 days
 }
+
